7b7d3e2ef706b0945eba217033ad3612f89ac335
cameliaandreea2605 <andreandreea2605@gmail.com>
2025-11-15 01:36:15 +0200

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,22 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { Toaster } from 'sonner'
 import Navbar from "./components/Navbar"
 import LoadingSpinner from "./components/LoadingSpinner"
 import useCheckToken from './hooks/useCheckToken'
+import { useEffect } from 'react'
 
 export default function App() {
+  const navigate = useNavigate();
   useCheckToken()
+  const loggedIn = useSelector(state => state.user.loggedIn)
   const checkTokenLoading = useSelector(state => state.user.checkTokenLoading)
+
+  useEffect(() => {
+    if (!loggedIn && !checkTokenLoading) {
+      navigate('/');
+    }
+  }, [loggedIn, checkTokenLoading, navigate]);
 
   if (checkTokenLoading) {
     return <LoadingSpinner />
